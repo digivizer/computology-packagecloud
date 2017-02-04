8c0d0c2690a5eb2feca00b2c24dad046db5bb4a4
Rob Sharp <qnm@fea.st>
2017-02-04 13:27:05 +1100

    Specify path for gem repo

--- manifests/gem_repo.pp
+++ manifests/gem_repo.pp
@@ -6,6 +6,7 @@ define packagecloud::gem_repo(
   exec { "install packagecloud ${repo_name} repo as gem source":
     command => "gem source --add ${base_url}/${repo_name}/",
     unless  => "gem source --list | grep ${base_url}/${repo_name}",
+    path    => "/usr/bin:/bin",
   }
 
 }
